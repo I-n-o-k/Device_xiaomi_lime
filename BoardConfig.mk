6736fab22b706ab97144b914f97f133a71fef2da
HeroBuxx <herobuxx@gmail.com>
2022-06-27 12:42:39 +0000

    lime: Pull libinit from sm8250-common  * Reff: https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common/tree/lineage-19.1/libinit
    Co-authored-by: boedhack99 <boedhack@gmail.com>
    Co-authored-by: frost. (frostg-012) <frost@nusantararom.org>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,9 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 ODM_MANIFEST_SKUS += lemon
 ODM_MANIFEST_LEMON_FILES := $(DEVICE_PATH)/manifest/manifest_lemon.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_lime
+TARGET_RECOVERY_DEVICE_MODULES := libinit_lime
+
 # Inherit from the proprietary version
 include vendor/xiaomi/lime/BoardConfigVendor.mk
